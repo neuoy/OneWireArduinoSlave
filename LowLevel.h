03c2f0afd3cfeb83868714cd0f01123f2cad870b
Youen Toupin <youen.toupin@gmail.com>
2015-04-23 17:26:33 +0200

    implemented reset detection

---
+++ LowLevel.h
@@ -0,0 +1,93 @@
+#ifndef _LowLevel_h
+#define _LowLevel_h
+
+#include <inttypes.h>
+
+#if ARDUINO >= 100
+#include "Arduino.h"       // for delayMicroseconds, digitalPinToBitMask, etc
+#else
+#include "WProgram.h"      // for delayMicroseconds
+#include "pins_arduino.h"  // for digitalPinToBitMask, etc
+#endif
+
+#if defined(__AVR__)
+#define PIN_TO_BASEREG(pin)             (portInputRegister(digitalPinToPort(pin)))
+#define PIN_TO_BITMASK(pin)             (digitalPinToBitMask(pin))
+#define IO_REG_TYPE uint8_t
+#define IO_REG_ASM asm("r30")
+#define DIRECT_READ(base, mask)         (((*(base)) & (mask)) ? 1 : 0)
+#define DIRECT_MODE_INPUT(base, mask)   ((*((base)+1)) &= ~(mask))
+#define DIRECT_MODE_OUTPUT(base, mask)  ((*((base)+1)) |= (mask))
+#define DIRECT_WRITE_LOW(base, mask)    ((*((base)+2)) &= ~(mask))
+#define DIRECT_WRITE_HIGH(base, mask)   ((*((base)+2)) |= (mask))
+
+#elif defined(__MK20DX128__) || defined(__MK20DX256__)
+#define PIN_TO_BASEREG(pin)             (portOutputRegister(pin))
+#define PIN_TO_BITMASK(pin)             (1)
+#define IO_REG_TYPE uint8_t
+#define IO_REG_ASM
+#define DIRECT_READ(base, mask)         (*((base)+512))
+#define DIRECT_MODE_INPUT(base, mask)   (*((base)+640) = 0)
+#define DIRECT_MODE_OUTPUT(base, mask)  (*((base)+640) = 1)
+#define DIRECT_WRITE_LOW(base, mask)    (*((base)+256) = 1)
+#define DIRECT_WRITE_HIGH(base, mask)   (*((base)+128) = 1)
+
+#elif defined(__SAM3X8E__)
+// Arduino 1.5.1 may have a bug in delayMicroseconds() on Arduino Due.
+// http://arduino.cc/forum/index.php/topic,141030.msg1076268.html#msg1076268
+// If you have trouble with OneWire on Arduino Due, please check the
+// status of delayMicroseconds() before reporting a bug in OneWire!
+#define PIN_TO_BASEREG(pin)             (&(digitalPinToPort(pin)->PIO_PER))
+#define PIN_TO_BITMASK(pin)             (digitalPinToBitMask(pin))
+#define IO_REG_TYPE uint32_t
+#define IO_REG_ASM
+#define DIRECT_READ(base, mask)         (((*((base)+15)) & (mask)) ? 1 : 0)
+#define DIRECT_MODE_INPUT(base, mask)   ((*((base)+5)) = (mask))
+#define DIRECT_MODE_OUTPUT(base, mask)  ((*((base)+4)) = (mask))
+#define DIRECT_WRITE_LOW(base, mask)    ((*((base)+13)) = (mask))
+#define DIRECT_WRITE_HIGH(base, mask)   ((*((base)+12)) = (mask))
+#ifndef PROGMEM
+#define PROGMEM
+#endif
+#ifndef pgm_read_byte
+#define pgm_read_byte(addr) (*(const uint8_t *)(addr))
+#endif
+
+#elif defined(__PIC32MX__)
+#define PIN_TO_BASEREG(pin)             (portModeRegister(digitalPinToPort(pin)))
+#define PIN_TO_BITMASK(pin)             (digitalPinToBitMask(pin))
+#define IO_REG_TYPE uint32_t
+#define IO_REG_ASM
+#define DIRECT_READ(base, mask)         (((*(base+4)) & (mask)) ? 1 : 0)  //PORTX + 0x10
+#define DIRECT_MODE_INPUT(base, mask)   ((*(base+2)) = (mask))            //TRISXSET + 0x08
+#define DIRECT_MODE_OUTPUT(base, mask)  ((*(base+1)) = (mask))            //TRISXCLR + 0x04
+#define DIRECT_WRITE_LOW(base, mask)    ((*(base+8+1)) = (mask))          //LATXCLR  + 0x24
+#define DIRECT_WRITE_HIGH(base, mask)   ((*(base+8+2)) = (mask))          //LATXSET + 0x28
+
+#else
+#error "Please define I/O register types here"
+#endif
+
+class Pin
+{
+private:
+	IO_REG_TYPE mask;
+	volatile IO_REG_TYPE *reg;
+
+public:
+	inline Pin(uint8_t pin)
+	{
+		mask = PIN_TO_BITMASK(pin);
+		reg = PIN_TO_BASEREG(pin);
+	}
+
+	inline void inputMode() { DIRECT_MODE_INPUT(reg, mask); }
+	inline void outputMode() { DIRECT_MODE_OUTPUT(reg, mask); }
+
+	inline bool read() { return DIRECT_READ(reg, mask); }
+	inline void writeLow() { DIRECT_WRITE_LOW(reg, mask); }
+	inline void writeHigh() { DIRECT_WRITE_HIGH(reg, mask); }
+	inline void write(bool value) { if (value) writeHigh(); else writeLow(); }
+};
+
+#endif
--- OneWireIO.ino
+++ OneWireIO.ino
@@ -1,18 +1,27 @@
 #include "Arduino.h"
+#include "LowLevel.h"
 #include "SerialChannel.h"
 
 #define LEDPin    13
-#define OWPin     2
+#define OWPin 2
 #define InterruptNumber 0 // Must correspond to the OWPin to correctly detect state changes. On Arduino Uno, interrupt 0 is for digital pin 2
+
+#define ResetMinDuration 360
+#define ResetMaxDuration 900
 
 SerialChannel debug("debug");
 
+Pin owPin(OWPin);
+Pin ledPin(LEDPin);
+
+unsigned long resetStart = (unsigned long)-1;
+
 void setup()
 {
-    pinMode(LEDPin, OUTPUT); 
-    pinMode(OWPin, INPUT);
+	ledPin.outputMode();
+	owPin.inputMode();
 
-    digitalWrite(LEDPin, LOW);
+	ledPin.writeLow();
 
     attachInterrupt(InterruptNumber,onewireInterrupt,CHANGE);
     
@@ -23,11 +32,28 @@ void loop()
 {
     cli();//disable interrupts
     
-	sei();//enable interrupts
+    sei();//enable interrupts
     
 }
 
 void onewireInterrupt(void)
 {
-    digitalWrite(LEDPin, digitalRead(OWPin));
+	bool state = owPin.read();
+	unsigned long now = micros();
+
+	//ledPin.write(state);
+
+	if (!state)
+		resetStart = now;
+
+	if (state)
+	{
+		unsigned long resetDuration = now - resetStart;
+		if (resetStart != (unsigned long)-1 && resetDuration >= ResetMinDuration && resetDuration <= ResetMaxDuration)
+		{
+			debug.write("reset");
+			ledPin.writeHigh();
+		}
+		resetStart = (unsigned long)-1;
+	}
 }
